31af2f145201f7e747605b07af47a4e08a99daba
Stephen Amos <amos.stephen.m@gmail.com>
2024-06-26 14:02:46 -0600

    add authRouter tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,37 @@
-test('hello world', () => {
-  expect('hello' + ' world').toBe('hello world');
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+let testUserId;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('logout', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+});
+
+test('updateUser', async () => {
+  const updateUserRes = await request(app)
+    .put('/api/auth/' + testUserId)
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send({ email: 'new@email', password: 'newpassword' });
+  expect(updateUserRes.status).toBe(200);
+  expect(updateUserRes.body.email).toBe('new@email');
 });
---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,14 @@
+const { DB, Role } = require('../database/database.js');
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+  return user;
+}
