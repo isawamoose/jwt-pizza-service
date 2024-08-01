be4a26f2b31d74833c1b387c4bcd16c0fe760c06
Stephen Amos <amos.stephen.m@gmail.com>
2024-08-01 13:23:29 -0600

    feat: add extra log sanitizer

--- src/logger.js
+++ src/logger.js
@@ -52,7 +52,9 @@ class Logger {
 
   sanitize(logData) {
     logData = JSON.stringify(logData);
-    return logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+    logData = logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+    logData = logData.replace(/\\password\\=\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+    return logData;
   }
 
   sendLogToGrafana(event) {
